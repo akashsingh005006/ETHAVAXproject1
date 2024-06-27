c36c1b78d290a6dfbaa749373a51ab2055eec3b5
akashsingh005006 <22akash03@gmail.com>
2024-06-27 18:31:05 +0530

    Update Module1project.sol

--- Module1project.sol
+++ Module1project.sol
@@ -1,55 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract expenseManagement{
+contract expenseManagement {
     address public person;
     uint public income;
     uint public expenditure;
     uint public savings;
 
-   event incomeReceived(address indexed account,uint amount);
-   event ExpenditureAmount(address indexed account,uint amount);
-   event savingUpdate(uint newSavings);
+    event incomeReceived(address indexed account, uint amount);
+    event ExpenditureAmount(address indexed account, uint amount);
+    event SavingUpdate(uint newSavings);
 
-   constructor() {
-    person=msg.sender;
-    savings=0;
- }
+    constructor() {
+        person = msg.sender;
+        savings = 0;
+    }
 
-function receiveIncome(uint amount) public {
-    require(msg. sender  == person, "Unauthorized");
-    require(amount > 0, "Sufficient Money");
+    function receiveIncome(uint amount) public {
+        require(msg.sender == person, "Unauthorized ");
+        require(amount > 0, "Sufficient Money");
 
-    income=income + amount;
-    savings=savings + amount;
+        if (income + amount > 10000) {
+            revert("Income exceeds taxless limit: Tax Applied");
+        }
 
-    emit incomeReceived(msg.sender,amount);
-    emit savingUpdate(savings);
+        income =income+ amount;
+        savings = savings + amount;
 
-    assert(savings >= income);
-}
+        emit incomeReceived(msg.sender, amount);
+        emit SavingUpdate(savings);
 
-function expenditureRecord(uint amount) public {
-    require(msg.sender == person ,"Unauthorized");
-    require(amount > 0 ,"expenditure must be positive ");
-    require(savings >= amount ,"insufficient savings");
+        assert(savings >= income);
+    }
 
-    expenditure = expenditure + amount;
-    savings = savings - amount; 
+    function expenditureRecord(uint amount) public {
+        require(msg.sender == person, "Unauthorized ");
+        require(amount > 0, "expenditure must be positive");
+        require(savings >= amount, "Insufficient savings");
 
-    emit ExpenditureAmount(msg.sender, amount);
-    emit savingUpdate(savings);
+        if (expenditure + amount > 5000) {
+            revert("Expenditure limits < 5000");
+        }
 
-    assert(savings >= 0);
-}
+        expenditure = expenditure + amount;
+        savings = savings - amount; 
 
-function remainingSaving() public view returns (uint) {
-    return savings;
+        emit ExpenditureAmount(msg.sender, amount);
+        emit SavingUpdate(savings);
 
-}
+        assert(savings >= 0);
+    }
 
-function disallowedOperation() public pure {
-    revert("operation not allowed");
-}
+    function remainingSaving() public view returns (uint) {
+        return savings;
+    }
 
+    function disallowedOperation() public pure {
+        revert("Operation not allowed");
+    }
+
+    function ConditionCheck() public view {
+        if (msg.sender!= person) {
+            revert("Unauthorized access:  owner has only access  to this function");
+        }
+        disallowedOperation();
+    }
 }
